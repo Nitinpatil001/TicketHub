9bc7c36e60f17141f094225b48f4c0e711588893
Nitin Patil <126043669+Nitinpatil001@users.noreply.github.com>
2024-11-15 09:58:36 +0530

    Update wsgi.py

--- ticketing_tool/wsgi.py
+++ ticketing_tool/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+# Add your project directory to the sys.path
+project_home = '/home/NitinPatil/Ticketing_Website'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Set the DJANGO_SETTINGS_MODULE to your settings file
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Ticketing_Website.settings'
+
+# Import the WSGI application from Django
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticketing_tool.settings')
-
 application = get_wsgi_application()
+
